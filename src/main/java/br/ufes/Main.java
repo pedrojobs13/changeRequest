8b567e5a6c17776f672abdfc3c536466035a9f85
HudrsonSilvaHS <155266342+sknnys@users.noreply.github.com>
2024-12-04 14:13:06 +0000

    implementações

--- src/main/java/br/ufes/Main.java
+++ src/main/java/br/ufes/Main.java
@@ -1,50 +1,71 @@
 package br.ufes;
 
 import br.ufes.log.ILog;
+import br.ufes.log.DBLog;
 import br.ufes.log.JSONLog;
 import br.ufes.log.XMLLog;
 import br.ufes.model.Cliente;
 import br.ufes.model.Item;
 import br.ufes.model.Pedido;
+import br.ufes.moduloDescontoEntrega.FormaCalculoDescontoEntregaValorPedidoImpl;
+import br.ufes.moduloDescontoEntrega.FormaDescontoTaxaPorTipoClienteImpl;
+import br.ufes.moduloDescontoEntrega.FormaDescontoTipoItemPresenteImpl;
 import br.ufes.service.UsuarioLogadoService;
 import br.ufes.util.GeradorDeLog;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        ILog log = new JSONLog();
+        try {
+            ILog log = new JSONLog();
 
-        Cliente cliente = new Cliente(
-                "Maria Oliveira",
-                "Pessoa Física",
-                "Centro",
-                "Ouro",
-                "Vitória",
-                "Rua das Flores, 123"
-        );
+            Cliente cliente = new Cliente(
+                    "Maria Oliveira",
+                    "Pessoa Física",
+                    "Centro",
+                    "Ouro",
+                    "Vitória",
+                    "Rua das Flores, 123"
+            );
 
+            double taxaEntrega = 15.0;
+            LocalDate dataPedido = LocalDate.now();
+            Pedido pedido = new Pedido(taxaEntrega, dataPedido, cliente);
 
-        double taxaEntrega = 15.0;
-        LocalDate dataPedido = LocalDate.now();
-        Pedido pedido = new Pedido(taxaEntrega, dataPedido, cliente);
+            Item item1 = new Item("Notebook", 1, 3000.0, "Eletrônicos");
+            Item item2 = new Item("Teclado", 2, 150.0, "Periféricos");
+            Item item3 = new Item("Cadeira Gamer", 1, 800.0, "Móveis");
+            pedido.adicionarItem(item1);
+            pedido.adicionarItem(item2);
+            pedido.adicionarItem(item3);
 
+            FormaCalculoDescontoEntregaValorPedidoImpl descontoValorPedido = new FormaCalculoDescontoEntregaValorPedidoImpl(250.0);
+            FormaDescontoTaxaPorTipoClienteImpl descontoTipoCliente = new FormaDescontoTaxaPorTipoClienteImpl();
+            FormaDescontoTipoItemPresenteImpl descontoTipoItem = new FormaDescontoTipoItemPresenteImpl();
 
-        Item item1 = new Item("Notebook", 1, 3000.0, "Eletrônicos");
-        Item item2 = new Item("Teclado", 2, 150.0, "Periféricos");
-        Item item3 = new Item("Cadeira Gamer", 1, 800.0, "Móveis");
+            Map<String, Double> descontosAplicados = descontoValorPedido.calcularDesconto(pedido);
+            descontosAplicados.putAll(descontoTipoCliente.calcularDesconto(pedido));
+            descontosAplicados.putAll(descontoTipoItem.calcularDesconto(pedido));
 
-        pedido.adicionarItem(item1);
-        pedido.adicionarItem(item2);
-        pedido.adicionarItem(item3);
+            double totalDesconto = descontosAplicados.values().stream().mapToDouble(Double::doubleValue).sum();
+            double valorTotalPedido = pedido.getValorPedido() - totalDesconto;
 
-        var gerar = GeradorDeLog.execute(UsuarioLogadoService.getNomeUsuario(), pedido.getId(), pedido.getValorPedido(), cliente.getNome());
+            String nomeMetodo = "getValorPedido";
+            String logMensagem = GeradorDeLog.execute(
+                    UsuarioLogadoService.getNomeUsuario(),
+                    pedido.getId(),
+                    nomeMetodo,
+                    pedido.getCliente().getNome(),
+                    valorTotalPedido
+            );
 
-        log.escrever(gerar);
-
-        log = new XMLLog();
-
-        log.escrever(gerar);
-
+            log.escrever(logMensagem);
+            
+        } catch (Exception e) {
+            System.err.println("Erro ao executar a aplicação: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/br/ufes/util/GeradorDeLog.java
+++ src/main/java/br/ufes/util/GeradorDeLog.java
@@ -4,23 +4,15 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class GeradorDeLog {
-    public static String execute(String nomeUsuario, String codigoPedido, Double nomeMetodo, String nomeCliente) {
-
+    public static String execute(String nomeUsuario, String codigoPedido, String nomeMetodo, String nomeCliente, double valorTotalPedido) {
         final LocalDateTime agora = LocalDateTime.now();
         final String data = agora.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         final String hora = agora.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
 
-        StringBuilder log = new StringBuilder();
-        log.append("{");
-        log.append("\"NOME_USUARIO\":\"").append(nomeUsuario).append("\",");
-        log.append("\"Data\":\"").append(data).append("\",");
-        log.append("\"Hora\":\"").append(hora).append("\",");
-        log.append("\"codigo_pedido\":\"").append(codigoPedido).append("\",");
-        log.append("\"Nome de Operação\":\"Calculo do valor total do pedido (").append(nomeMetodo).append(")\",");
-        log.append("\"Nome_Cliente\":\"").append(nomeCliente).append("\"");
-        log.append("}");
 
-
-        return log.toString();
+        return String.format(
+            "NOME_USUARIO: %s | Data: %s | Hora: %s | codigo_pedido: %s | Nome de Operação: Calculo do valor total do pedido (%s) | Nome_Cliente: %s | ValorTotalPedido (%.2f)\n",
+            nomeUsuario, data, hora, codigoPedido, nomeMetodo, nomeCliente, valorTotalPedido
+        );
     }
 }
